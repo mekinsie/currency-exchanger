87eebf93e472590ecf5e6aefc533bb30b018c334
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-02-12 10:36:11 -0800

    Create a DollarConversion service using async function and fetch.

---
+++ src/services/conversion-service.js
@@ -0,0 +1,14 @@
+export default class DollarConversion {
+  static async getConversion(fromCurrency) {
+    try {
+      const convertResponse = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${fromCurrency}`);
+      if (!convertResponse.ok) {
+        throw Error(convertResponse.statusText)
+      } else {
+      return convertResponse.json();
+      }
+    } catch(error) {
+      return convertResponse.message;
+    }
+  }
+}
