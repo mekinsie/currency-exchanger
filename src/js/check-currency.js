73578da6c145aaca709286571361cddd1247ac2d
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-02-12 14:25:43 -0800

    Edit checkCurrency function to look for unsupported-code message in error.

--- src/js/check-currency.js
+++ src/js/check-currency.js
@@ -1,9 +1,9 @@
 export function checkCurrency(conversionResponse, toCurrency,fromCurrency) {
-  if (conversionResponse.result && conversionResponse.result === "error") {
-    return (`${fromCurrency} is not a currency in our database.`)
+  if (conversionResponse.result && conversionResponse.result["error-type"] === "unsupported-code") {
+    return (`${fromCurrency} is not a currency in our database.`);
   }
   else if (toCurrency in conversionResponse.conversion_rates === false) {
-    return (`${toCurrency} is not a currency in our database.`)
+    return (`${toCurrency} is not a currency in our database.`);
   } else {
     let conversionRate = conversionResponse.conversion_rates[toCurrency];
     return (`${conversionRate} ${toCurrency}`);
